7b1f1d484f84606d690e72b023c02de21cc1ec40
richiever <richie206080@gmail.com>
2023-10-24 14:57:11 +1100

    Setup basic GET functions

--- get_test.go
+++ get_test.go
@@ -5,14 +5,25 @@ import (
 	"testing"
 )
 
-var WantVal []byte = R.Get()
+type Requests1 struct {
+	arg1 []byte
+	arg2 Requests
+}
 
 var R = Requests{"https://jsonplaceholder.typicode.com/posts/1"}
+var WantVal []byte = R.Get()
+var Allr = []Requests1{
+	Requests1{arg1: R.Get(), arg2: R},
+}
 
 func Test_Get(t *testing.T) {
-	got := R.Get()
-	want := WantVal
-	if !bytes.Equal(got, want) {
-		t.Errorf("wanted %s, got %s", want, got)
+
+	for _, tests := range Allr {
+		got := tests.arg2.Get()
+		want := tests.arg1
+		if !bytes.Equal(got, want) {
+			t.Errorf("wanted %s, got %s", want, got)
+		}
 	}
+
 }
